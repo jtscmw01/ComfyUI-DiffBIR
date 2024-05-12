e8ee8daab033fbfb346d608d93454cca0f7b4211
Jtscmw01 <jtscmw@163.com>
2024-05-12 20:05:25 +0800

    advanced sampler

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
-from .diffbir.sampler import DiffBIR_sample
+from .diffbir.sampler import DiffBIR_sample, DiffBIR_sample_advanced
 from .diffbir.load import Stage2_load, Stage1_load, Simple_load
 
 NODE_CLASS_MAPPINGS = {
     "DiffBIR_sample": DiffBIR_sample,
+    "DiffBIR_sample_advanced": DiffBIR_sample_advanced,
     "Stage1_load": Stage1_load,
     "Stage2_load": Stage2_load,
     "Simple_load": Simple_load,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DiffBIR_sample": "DiffBIR Sampler",
+    "DiffBIR_sample_advanced": "DiffBIR Sampler (Advanced)",
     "Stage1_load": "DiffBIR stage1 loader",
     "Stage2_load": "DiffBIR stage2 loader",
     "Simple_load": "DiffBIR simple load",
--- diffbir/sampler.py
+++ diffbir/sampler.py
@@ -28,7 +28,7 @@ def check_device(device: str) -> str:
 
 
 
-class DiffBIR_sample:
+class DiffBIR_sample_advanced:
 
     def __init__(self):
         pass
@@ -121,6 +121,96 @@ class DiffBIR_sample:
             g_stop=g_stop,
             g_space=g_space,
             g_repeat=g_repeat,
+            output='',
+            seed=seed,
+            device=device
+        )
+
+        pipe = BSRInferenceLoop(args)#, stage1_model, cldm, diffusion)
+        pipe.init_stage1_model(stage1_model)
+        pipe.init_stage2_model(cldm, diffusion)
+        pipe.init_pipeline()
+
+        image = pipe.run()
+        
+        return (image,)
+    
+
+class DiffBIR_sample:
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "stage1_model": ("STAGE1",),
+            "cldm": ("CLDM",),
+            "diffusion": ("DIFFUSION",),
+            "image": ("IMAGE",),
+            "upscale_ratio": ("FLOAT", {"default": 2, "min": 0.1, "max": 8.0, "step": 0.1}),
+            "steps": ("INT", {"default": 20, "min": 1, "max": 0xffffffffffffffff, "step": 1}),
+            "cfg": ("FLOAT", {"default": 4.0, "min": 0, "max": 100, "step": 0.1}),
+            "better_start": ("BOOLEAN", {"default": True}),
+            "tiled": ("BOOLEAN", {"default": True}),
+            "tile_size": ("INT", {"default": 512, "min": 1, "max": 0xffffffffffffffff, "step": 1}),
+            "tile_stride": ("INT", {"default": 256, "min": 1, "max": 0xffffffffffffffff, "step": 1}),
+            "pos_prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            "neg_prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            "seed": ("INT", {"default": 123, "min": 0, "max": 0xffffffffffffffff, "step": 1}),
+            "device": (
+                    [
+                        'cuda',
+                        'cpu',
+                        'mps',
+                    ], {
+                        "default": 'cuda'
+                    }),
+
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "sample"
+    CATEGORY = "DiffBIR"
+    DESCRIPTION = """"""
+
+    def sample(self, stage1_model, cldm, diffusion, image, upscale_ratio, steps, cfg, better_start, tiled, tile_size, tile_stride, pos_prompt, neg_prompt, 
+               seed, device):
+        device = check_device(device)
+        print(image.shape)
+
+        # stage1 tile by resolution
+        stage1_tile = False
+        if image.shape[1] * image.shape[2] > 768 * 1024:
+            stage1_tile = True
+
+        # 创建一个Namespace对象
+        args = argparse.Namespace(
+            task='sr',
+            upscale=upscale_ratio,
+            version='v2',
+            steps=steps,
+            better_start=better_start,
+            tiled=tiled,
+            tile_size=tile_size,
+            tile_stride=tile_stride,
+            stage1_tile=stage1_tile,
+            stage1_tile_size=512,
+            stage1_tile_stride=480,
+            pos_prompt=pos_prompt,
+            neg_prompt=neg_prompt,
+            cfg_scale=cfg,
+            input=image,
+            n_samples=1,
+            guidance=False,
+            g_loss="w_mse",
+            g_scale=1.00,
+            g_start=1001,
+            g_stop=-1,
+            g_space="latent",
+            g_repeat=1,
             output='',
             seed=seed,
             device=device
